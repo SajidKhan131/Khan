86572c5c242228d5c6ac43883e1a0fd9ab79fffd
Sajid Ullah <usajid95@gmail.com>
2016-05-30 17:49:16 +0500

    Build documentation

---
+++ BuildP/src/Build.java
@@ -0,0 +1,17 @@
+
+public class Build {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x,y,result ;
+		x=5;
+		y=10;
+		
+		result= x +y;
+		
+		System.out.println("Result :" + result);
+
+	}
+
+}
